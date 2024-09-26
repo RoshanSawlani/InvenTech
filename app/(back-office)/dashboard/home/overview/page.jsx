b4e88684160ec7feecd0af0cd6d1594450c8fa63
RoshanSawlani <roshansawlani@gmail.com>
2024-09-26 15:32:27 +0530

    Added DashboardBanner component to promote online payments with dismiss functionality

--- app/(back-office)/dashboard/home/overview/page.jsx
+++ app/(back-office)/dashboard/home/overview/page.jsx
@@ -1,9 +1,11 @@
+import DashboardBanner from '@/components/dashboard/DashboardBanner'
 import React from 'react'
 
 export default function Dashboard() {
     return (
         <div>
-            <h2>Overview</h2>
+            <DashboardBanner/>
+            <h2>Dashboard</h2>
         </div>
         
     )
